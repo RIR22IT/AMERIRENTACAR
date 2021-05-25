715efbc7e5493e9abb8e8125149d5ba7a40842c6
Thisara <thisaradilshanbens99@gmail.com>
2021-05-25 15:50:01 +0530

    changes done

--- inc/mobile-slider.php
+++ inc/mobile-slider.php
@@ -2,50 +2,64 @@
         <!-- ==========================-->
         <div data-off-canvas="mobile-slidebar left overlay">
             <a class="navbar-brand scroll" href="home.html">
-                <img class="normal-logo img-resonsive visible-xs visible-sm" src="assets/media/general/logo.png" alt="logo" />
-                <img class="scroll-logo img-resonsive hidden-xs hidden-sm" src="assets/media/general/logo-dark.png" alt="logo" />
+                <img class="normal-logo img-resonsive visible-xs visible-sm" src="./images/logo-new_withcap.png" alt="logo" />
+                <img class="scroll-logo img-resonsive hidden-xs hidden-sm" src="./images/logo-new_withcap.png" alt="logo" />
             </a>
             <ul class="nav navbar-nav">
                 <li>
                     <h4><a href="">Mobile sidebar</a></h4>
                 </li>
-                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Home<b class="caret"></b></a>
+                <li class="dropdown"><a class="dropdown-toggle" href="index.php" data-toggle="dropdown">Home</a>
+                </li>
+                <li><a href="who-we-are.php">Who we are</a>
+                </li>
+                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Vehicle Fleet<b class="caret"></b></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li><a href="home.html" tabindex="-1">Home one page</a>
-                        </li>
-                        <li><a href="home-2.html" tabindex="-1">Home standart</a>
-                        </li>
+                                <li><a href="rent-general-cars.php" tabindex="-1">RENT GENERAL CARS IN SRI LANKA</a>
+                                </li>
+                                <li><a href="premium-cars-for-rental.php" tabindex="-1">PREMIUM CARS FOR RENT IN SRI LANKA</a>
+                                </li>
+                                <li><a href="4WD-for-rent.php" tabindex="-1">SUV/4WD FOR RENT IN SRI LANKA</a>
+                                </li>
+                                <li><a href="wedding-cars-for-rent.php" tabindex="-1">WEDDING CARS FOR RENT IN SRI LANKA</a>
+                                </li>
+                                <li><a href="classic-and-vintage-cars.php" tabindex="-1">CLASSIC AND VINTAGE CARS</a>
+                                </li>
+                                <li><a href="luxury-vans-for-rent.php" tabindex="-1">LUXURY VANS FOR RENT IN SRI LANKA</a>
+                                </li>
+                                <li><a href="luxury-buses-for-rent.php" tabindex="-1">LUXURY BUSES FOR RENT IN SRI LANKA</a>
+                                </li>
                     </ul>
                 </li>
-                <li><a href="repair-shop.html">Repair Shop</a>
+                <li><a href="car-rental.html">Our Cars</a>
                 </li>
-                <li><a href="car-rental.html">Car Rental</a>
+                <li><a href="our-location.php">Our Location</a>
                 </li>
-                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Listings<b class="caret"></b></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="listings-1.html" tabindex="-1">Listings ver 01</a>
-                        </li>
-                        <li><a href="listings-2.html" tabindex="-1">Listings ver 02</a>
-                        </li>
-                        <li><a href="listings-3.html" tabindex="-1">Listings ver 03</a>
-                        </li>
-                    </ul>
+                <li class="dropdown"><a class="dropdown-toggle" href="listings-1.html"
+                                data-toggle="dropdown">Rates<b class="caret"></b></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="self-drive-rates.php" tabindex="-1">SELF DRIVE RATES IN SRI LANKA</a>
+                                </li>
+                                <li><a href="with-drive-rates.php" tabindex="-1">WITH DRIVER RATES</a>
+                                </li>
+                                <li><a href="wedding-car-rates.php" tabindex="-1">WEDDING CAR RATES IN SRI LANKA</a>
+                                </li>
+                                <li><a href="motorbike-rates.php" tabindex="-1">MOTORBIKE & BICYCLE RENTAL</a>
+                                </li>
+                            </ul>
                 </li>
-                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Blog<b class="caret"></b></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="blog-main.html" tabindex="-1">Blog main</a>
-                        </li>
-                        <li><a href="blog-post.html" tabindex="-1">Blog post</a>
-                        </li>
-                    </ul>
+                <li><a href="https://www.distancesfrom.com/">Distance Calculator</a>
                 </li>
-                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Pages<b class="caret"></b></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="car-details.html" tabindex="-1">Car details</a>
-                        </li>
-                        <li><a href="typography.html" tabindex="-1">Typography</a>
-                        </li>
-                    </ul>
+                <li><a href="https://www.eta.gov.lk/slvisa/">E-Visa</a>
+                </li>
+                <li class="dropdown"><a class="dropdown-toggle" href="our-terms.php" data-toggle="dropdown">Our
+                                Terms<b class="caret"></b></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="our-terms.php" tabindex="-1">Our Terms</a></li>
+                                <li><a href="car-details.html" tabindex="-1">FAQ</a></li>
+                            </ul>
+                </li>
+                <li><a href="contact.php">Contact</a>
                 </li>
             </ul>
         </div>
