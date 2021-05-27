3998d7b1c40924e056269263f51d0339e0b0d22c
THARUSHIKA <it19016108@my.sliit.lk>
2021-05-27 15:52:39 +0530

    form added

---
+++ checkout_connection.php
@@ -0,0 +1,28 @@
+<?php
+include('./database/connection.php')
+
+
+
+//Insert data into the db
+
+if(isset($_POST['book'])){
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $companyname = $_POST['companyname'];
+    $country = $_POST['country'];
+    $streat = $_POST['streat'];
+    $unit = $_POST['unit'];
+    $city = $_POST['city'];
+    $postcode = $_POST['postcode'];
+    $phone = $_POST['phone'];
+    $emailaddress = $_POST['emailaddress'];
+    $information = $_POST['information'];
+
+    $qry = "INSERT INTO checkout (firstname, lastname, companyname, country, streat, unit, city, postcode, phone, emailaddress, information) 
+    VALUES ('$firstname', '$lastname', '$companyname', '$country', '$streat', '$unit','$city', '$postcode', '$phone', '$emailaddress', '$information')"; 
+    $run = mysqli_query($db, $qry);
+    $_SESSION['message'] = "Added successfully"; 
+}
+
+
+?>
