783685f39367c0267240c64a1d527a4271fcf628
THARUSHIKA <it19016108@my.sliit.lk>
2021-05-28 18:04:11 +0530

    email page

---
+++ email.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/styles.css" />
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="mail">
+    <div class = "cover"></div>
+    <div class="letter"><h5>Thank you for Booking!</h5></div>
+    </div>
+</div>
+<div class="button">
+     <a href="index.php"><button class="btn btn-primary">BACK TO HOME</button></a>
+</div>
+</body>
+</html>
